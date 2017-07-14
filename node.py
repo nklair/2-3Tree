20cc0adcc4d71424f0d36deeecd0f3afeb4f8842
Nathan Klair <nklair@nd.edu>
2017-07-13 21:51:59 -0500

    Worked on insert and promote functions, as well as a print function for testing

--- node.py
+++ node.py
@@ -1,42 +1,78 @@
-
-
-
 class Node:
 	def __init__(self, size, parent):
 		self.maxSize = size
 		self.values = []
 		self.children = []
 		i = 0
-		while i < self.maxSize / 2:
+		while i <= self.maxSize:
 			self.children.append(None)
 			i += 1
 		self.parent = parent
 
-	def insert(self, value, newChild=None):
+	def insert(self, value, leftChild, rightChild):
 		valIndex = 0
 		while valIndex < len(self.values):
 			if self.values[valIndex] > value:
 				break
 			valIndex += 1
 		self.values.insert(valIndex, value)
-		if newChild == None:
-			self.children.append(newChild)
-		else:
-			newChildValue = newChild.getValues()[0]
-			valIndex = 0
-			while valIndex < len(self.values):
-				if self.values[valIndex] > newChildValue:
-					break
-				valIndex += 1
-			self.children.insert(valIndex, newChild)
+		# Make sure the children know their parent
+		if leftChild != None:
+			leftChild.parent = self
+		if rightChild != None:
+			rightChild.parent = self
+		# Left needs to replace old child
+		self.children[valIndex] =  leftChild
+		#right needs to be inserted in children list
+		self.children.insert(valIndex + 1,rightChild)
+		self.children.pop()
 
 		if len(self.values) >= self.maxSize:
 			self.promote()
 
 	def promote(self):
-		pass
+		indToPromote = int(self.maxSize / 2)
+		val = self.values[indToPromote]
+		# Don't know who the new children's parent will be yet
+		rightChild = Node(self.maxSize, None)
+		leftChild = Node(self.maxSize, None)
+		i = 0
+		while i < len(self.values):
+			if(i < indToPromote):
+				leftChild.insert(self.values[i], self.children[i], self.children[i + 1])
+			if(i > indToPromote):
+				rightChild.insert(self.values[i], self.children[i], self.children[i + 1])
+			i += 1
 
-	def getValues(self):
-		pass
+		self.parent.insert(val, leftChild, rightChild)
+
+	def print(self):
+		print(str(self.values))
+		print(str(self.children))
+		for child in self.children:
+			if(child != None):
+				print(child.print())
+
+# Node to handle the case when a new node needs to be created as the root
+class RootNode:
+	def __init__(self, size):
+		self.child = None
+		self.maxSize = size
+
+	def insert(self, value, leftChild, rightChild):
+		newRoot = Node(self.maxSize, self)
+		# Make sure the children know their parent
+		if leftChild != None:
+			leftChild.parent = newRoot
+		if rightChild != None:
+			rightChild.parent = newRoot
+		newRoot.insert(value, leftChild, rightChild)
+		self.child = newRoot
+
+	def print(self):
+		if(self.child != None):
+			self.child.print()
+
+
 
 
